b54901668c1228540d206974a541f12c6c4d5264
Ben Tennyson <vbalupraveen@gmail.com>
2019-10-21 17:34:52 -0500

    updated gd

--- week3/logistic.py
+++ week3/logistic.py
@@ -3,43 +3,31 @@ import numpy as np
 from week3.sigmoid import sigmoid
 
 
-def cost(theta, x, y):
+def cost(theta, X, y):
     m = len(y)
-    ones = np.ones((m, 1))
-    xi = np.transpose(np.hstack((ones, x)))
-    theta_t = np.transpose(theta)
-    thetat_x = np.dot(theta_t, xi)
+    thetat_x = np.dot(X, theta)
     h_theta_of_xi = sigmoid(thetat_x)
-    j_of_theta = (1 / m) * np.sum(-y * np.log(h_theta_of_xi) - (1 - y) * np.log(1 - h_theta_of_xi))
+    j_of_theta = (1 / m) * np.sum(-np.multiply(y, np.log(h_theta_of_xi)) - np.multiply((1 - y), np.log(1 - h_theta_of_xi)))
     return j_of_theta
 
 
-def derivativeOfTheta(theta, x, y):
+def cost_derivative(theta, X, y):
     m = len(y)
-    ones = np.ones((m, 1))
-    xi = np.transpose(np.hstack((ones, x)))
-    thetaT = np.transpose(theta)
-    thetaTOfX = np.dot(thetaT, xi)
-    h_theta_of_xi = sigmoid(thetaTOfX)
-    d_theta = (1 / m) * (np.sum(h_theta_of_xi - y) * xi)
-    return d_theta
-
-
-def gd(theta, x, y, no_of_iterations):
-    for i in range(0, no_of_iterations):
-        theta = theta - derivativeOfTheta(theta, x, y)
+    thetat_x = np.dot(X, theta)
+    g_minus_y = sigmoid(thetat_x) - y
+    theta = (1 / m) * np.dot(X.T, g_minus_y)
     return theta
 
 
 data = np.loadtxt('ex2data1.txt', delimiter=',')
-x = data[:, 0: 2]
 y = data[:, 2]
 m = len(y)
-nOfFeat = len(x[0])
+X = np.hstack((np.ones((m, 1)), data[:, 0: 2]))
+nOfFeat = len(X[0])
 y = y.reshape(m, 1)
-theta = np.zeros((nOfFeat + 1, 1))
-print(cost(theta, x, y))
+theta = np.zeros((nOfFeat, 1))
 print('------------------------------------------------')
-print(gd(theta, x, y, 100))
+print(cost(theta, X, y))
+print(cost_derivative(theta, X, y))
 print('------------------------------------------------')
-
+exit(0)
