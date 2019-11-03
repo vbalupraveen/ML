59bd52fc2b6f39c4c6c44df8909181f7a2028004
Ben Tennyson <vbalupraveen@gmail.com>
2019-11-03 15:55:09 -0600

    Update logistic.py

--- week3/logistic.py
+++ week3/logistic.py
@@ -4,6 +4,10 @@ from scipy.optimize import fmin_bfgs
 from week3.sigmoid import sigmoid
 
 
+def fmin_gradient(theta, X, y):
+    return fmin_bfgs(f=cost, x0=theta.flatten(), fprime=cost_derivative, args=(X, y.flatten()))
+
+
 def cost(theta, X, y):
     m = len(y)
     thetat_x = np.dot(X, theta)
@@ -30,30 +34,18 @@ def feature_scaling(X):
 
 
 def gradient(alpha, iterations, theta, X, y):
+    m = len(y)
     for _ in range(0, iterations):
         theta = theta - (alpha / m) * cost_derivative(theta, X, y)
-    return theta;
+    return theta
 
 
-def fmin_gradient(theta, X, y):
-    return fmin_bfgs(f=cost, x0=theta.flatten(), fprime=cost_derivative, args=(X,y.flatten()))
-
-
-data = np.loadtxt('ex2data1.txt', delimiter=',')
-y = data[:, 2]
-m = len(y)
-X = np.hstack((np.ones((m, 1)), data[:, 0: 2]))
-nOfFeat = len(X[0])
-y = y.reshape(m, 1)
-theta = np.zeros((nOfFeat, 1))
-print('--------------cost function--------------')
-print(cost(theta, X, y))
-print('--------------differentiation of Cost--------------')
-print(cost_derivative(theta, X, y))
-print('--------------feature normalized training set--------------')
-print(feature_scaling(X))
-print('--------------gradient descent--------------')
-print(gradient(0.1, 400, theta, X, y))
-print('--------------fmin gradient descent--------------')
-print(fmin_gradient(theta, X, y))
-exit(0)
+def init_values(data):
+    y = data[:, 2]
+    m = len(y)
+    X = data[:, 0: 2]
+    X = np.hstack((np.ones((m, 1)), X))
+    y = y.reshape(m, 1)
+    nOfFeat = len(X[0])
+    theta = np.zeros((nOfFeat, 1))
+    return theta, X, y
